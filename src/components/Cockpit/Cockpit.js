071c3e0ad1958110787c4d2f2f003e3512492aaf
Justine Kay <justine.b.kitagorsky@accenture.com>
2019-08-08 15:48:28 -0400

    Class-based vs functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -18,7 +18,7 @@ const cockpit = (props) => {
     
     return (
         <div className={styles.Cockpit}>
-          <h1>Helloooo, React app here.</h1>
+          <h1>{props.title}</h1>
           <p className={classes.join(' ')}>This is really working!</p>
           <button
           className={buttonClass} 
